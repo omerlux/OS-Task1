0a1c9e1c2401146f08bbc699e7be8e90ddc57fc0
eranaf <eranaf@gmail.com>
2020-11-11 18:26:54 +0200

    fix tee (include + exit)

--- xv6-public/tee.c
+++ xv6-public/tee.c
@@ -1,20 +1,22 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-    
+
+#include "fcntl.h"
+
  //passing command line arguments 
     
 int main(int argc, char *argv[]) 
 {
     if(argc == 2){
         char buf_read[512]; // creates a buffer
-        int fd_txt1 = open(argv[1], O_WRONLY | O_CREAT);    // create new fd if not exists, to write only
+        int fd_txt1 = open(argv[1], O_WRONLY | O_CREATE);    // create new fd if not exists, to write only
         while(1){
-            n = read(0, buf_read, sizeof(buf_read));    // return number of bytes
+            int n = read(0, buf_read, sizeof(buf_read));    // return number of bytes
             if(n==0) break; // end of file
             if(n<0) { // error in read
                 printf(2, "read error\n");
-                exit(0);
+                exit();
             }
             if(write(fd_txt1, buf_read, n) != n){ // wrriten bytes not equal to n = error!
                 printf(2, "write error\n");
