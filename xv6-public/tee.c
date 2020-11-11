fc8208923981a8e609fe1a64bfbcc4286b6a086f
eranaf <eranaf@gmail.com>
2020-11-11 19:29:43 +0200

    finish tee program

--- xv6-public/tee.c
+++ xv6-public/tee.c
@@ -14,19 +14,38 @@ int main(int argc, char *argv[])
         while(1){
             int n = read(0, buf_read, sizeof(buf_read));    // return number of bytes
             if(n==0) break; // end of file
-            if(n<0) { // error in read
+            else if(n<0) { // error in read
                 printf(2, "read error\n");
                 exit();
             }
-            if(write(fd_txt1, buf_read, n) != n){ // wrriten bytes not equal to n = error!
-                printf(2, "write error\n");
+            else if(n>= 2 && buf_read[n-2]==3) break;// cntr + c 
+            else {//write to file and console
+                if (write(fd_txt1, buf_read, n) != n) { // wrriten bytes not equal to n = error!
+                    printf(2, "write error\n");
+                }
+                if (write(1, buf_read, n) != n) { // wrriten bytes not equal to n = error!
+                    printf(2, "write to console error\n");
+                }
             }
-            if(write(1, buf_read, n) != n){ // wrriten bytes not equal to n = error!
-                printf(2, "write to console error\n");
-            }
+
         }
     }else if(argc == 3){
-
+        char buf_read[512]; // creates a buffer
+        int fd_txt1 = open(argv[1], O_RDONLY);
+        int fd_txt2 = open(argv[2], O_WRONLY | O_CREATE);    // create new fd if not exists, to write only
+        while(1){
+            int n = read(fd_txt1, buf_read, sizeof(buf_read));    // return number of bytes
+            if(n==0) break; // end of file
+            else if(n<0) { // error in read
+                printf(2, "read error\n");
+                exit();
+            }
+            else {//write to file and console
+                if (write(fd_txt2, buf_read, n) != n) { // wrriten bytes not equal to n = error!
+                    printf(2, "write error\n");
+                }
+            }
+        }
     }else
         printf(2, "cannot tee, given %d arguments\n", argc);
     exit();
