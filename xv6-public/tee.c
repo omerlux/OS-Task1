51c85d3c607114d93d104ded84e873c7dc1649d1
omerlux <omerlux1994@gmail.com>
2020-11-12 19:58:42 +0200

    infrastructure to getpinfo system call

--- xv6-public/tee.c
+++ xv6-public/tee.c
@@ -15,12 +15,14 @@ int main(int argc, char *argv[])
         int fd_txt1 = open(argv[1], O_WRONLY | O_CREATE);    // create new fd if not exists, to write only
         while(1){
             int n = read(0, buf_read, sizeof(buf_read));    // return number of bytes
-            if(n==0) break; // end of file
+            if(n==0)
+                break; // end of file
             else if(n<0) { // error in read
                 printf(2, "read error\n");
                 break;
             }
-            else if(n>= 2 && buf_read[n-2]==3) break;// ctrl + c    # TODO: how to implement a signal ctrl+c?
+            else if(n>= 2 && buf_read[n-2]==3)
+                break;// ctrl + c    # TODO: how to implement a signal ctrl+c?
             else {//write to file and console
                 if (write(fd_txt1, buf_read, n) != n) { // wrriten bytes not equal to n = error!
                     printf(2, "write error\n");
@@ -41,7 +43,8 @@ int main(int argc, char *argv[])
         int fd_txt2 = open(argv[2], O_WRONLY | O_CREATE);    // create new fd if not exists, to write only
         while(1){
             int n = read(fd_txt1, buf_read, sizeof(buf_read));    // return number of bytes
-            if(n==0) break; // end of file
+            if(n==0)
+                break; // end of file
             else if(n<0) { // error in read
                 printf(2, "read error\n");
                 break;
