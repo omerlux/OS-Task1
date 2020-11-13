9d9ac1a93ba6232cef49d51e4ae19159666734ef
omerlux <omerlux1994@gmail.com>
2020-11-13 12:43:38 +0200

    exit started

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -225,7 +225,7 @@ fork(void)
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
 void
-exit(void)
+exit(int status)    // 13.11 changed from void to int
 {
     struct proc *curproc = myproc();
     struct proc *p;
--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  // -- start edit 13/11 --
+  int status;                  // added status as int
+  // -- end edit 13/11 --
 };
 
 // Process memory is laid out contiguously, low addresses first:
